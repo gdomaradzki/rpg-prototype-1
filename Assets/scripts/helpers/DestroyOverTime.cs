54c5ea5826cce770abfd9615cd6bf8742913d860
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-05-15 15:12:01 -0300

    Created a scripts folder and moved controllers folder inside. Created DestroyOverTime script

---
+++ Assets/scripts/helpers/DestroyOverTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOverTime : MonoBehaviour {
+    // Time in seconds to autodestroy
+    public float destroyTime;
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+        destroyTime -= Time.deltaTime;
+        if (destroyTime <= 0) {
+            Destroy(gameObject);
+        }
+	}
+}
